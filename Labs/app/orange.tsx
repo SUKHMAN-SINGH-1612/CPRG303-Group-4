9a3a327bd2485679c6141d06582f71632c8bbb5c
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2025-01-28 11:45:28 -0700

    Update orange.tsx

--- Labs/app/orange.tsx
+++ Labs/app/orange.tsx
@@ -5,7 +5,7 @@ const Orange = () => {
   return (
     <View>
       <Text>Orange</Text>
-      <Image source={{ uri: 'https://example.com/orange.jpg' }} style={{ width: 100, height: 100 }} />
+      <Image source={{ uri: 'https://t3.ftcdn.net/jpg/02/01/79/54/360_F_201795443_a16jojvPRiobPC96uO7ePHSNzGVoBLbc.jpg' }} style={{ width: 100, height: 100 }} />
     </View>
   );
 };
