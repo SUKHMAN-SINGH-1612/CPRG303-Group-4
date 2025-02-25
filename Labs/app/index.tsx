6ce06316bcc26df5ebbb3d0ad14c49de3d7f8b4f
Sukhman Singh <sukhmansinghbhogal@gmail.com>
2025-02-25 12:59:11 -0700

    margin adjustment in index.tsx

--- Labs/app/index.tsx
+++ Labs/app/index.tsx
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     borderRadius: 10, 
     alignItems: 'center',
     justifyContent: 'center',
+    marginVertical: 10,
   },
   buttonText: {
     color: 'white', 
