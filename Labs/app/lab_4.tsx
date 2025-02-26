44957873230f6bf2d2f4f64d4e7549ce77f850bb
zarishbilal <zarishsuleman@gmail.com>
2025-02-26 10:20:37 -0700

    changed color

--- Labs/app/lab_4.tsx
+++ Labs/app/lab_4.tsx
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 20,
-    backgroundColor: '#001F54',
+    backgroundColor: '#0272ab',
     padding: 12,
     borderRadius: 10,
   },
